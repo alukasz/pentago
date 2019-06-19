c8fcbdf726017d1e0a717767dd205e18c4a511c6
Łukasz Antończyk <antonczyklukasz@gmail.com>
2019-06-19 13:37:30 +0200

    Update configuration for deployment

--- config/releases.exs
+++ config/releases.exs
@@ -1,3 +1,5 @@
 import Config
 
-config :pentago_web, Pentago.Web.Endpoint, server: true
+config :pentago_web, Pentago.Web.Endpoint,
+  server: true,
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
